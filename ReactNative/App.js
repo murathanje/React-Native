e5630234999a34a67cf0702c6603936e3354e377
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-06-20 17:01:26 +0300

    flex direction was done

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -6,24 +6,41 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.box1}></View>
-      <View style={styles.box2}></View>
+      <View style={styles.box2}>
+        <View style= {styles.child1}></View>
+        <View style= {styles.child2}></View>
+        <View style= {styles.child3}></View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.9,
+    flex: 1,
     backgroundColor: 'yellow',
     },
   box1: {
     backgroundColor: "gray",
-    flex: 0.5,
+    flex: 1,
 
   },
   box2: {
     backgroundColor: "bisque",
-    flex: 0.5,
+    flex: 1,
+    flexDirection: "row"
+  },
+  child1: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  child2: {
+    flex: 2,
+    backgroundColor: "red"
+  },
+  child3: {
+    flex: 1,
+    backgroundColor: "#140156"
   }
 
 });
