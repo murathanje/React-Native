2e7c67753e4ee69c40c4d26b0edcdee631e162a4
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-07-25 16:54:30 +0300

    Navigation was begin

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -1,48 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from "react";
-import Title from "./src/components/Title.js"
-import Header from "./src/components/Header.js"
-import User from "./src/components/User.js"
-import Users from "./src/components/Users.js"
-import ListItem from './src/components/ListItem.js';
-import { 
-  Button, 
-  StyleSheet, 
-  Text, 
-  View,
-  TouchableOpacity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-  Image,
-  FlatList,
-  SafeAreaView,
-  RefreshControl
-} from 'react-native';
-import Counters from './src/components/Counters.js';
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/Home';
+import UsersScreen from './src/screens/Users';
 
 
-const App = () => {
-  const [isvisible,setIsVisible] = useState(true);
+const Stack = createNativeStackNavigator();
 
+function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      {isvisible && <Counters />}
-      <Button title='Göster/Gizle' onPress={()=>setIsVisible(!isvisible)}/>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          title: 'Anasayfa'
+        }}/>
+        <Stack.Screen name="Users" component={UsersScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  click: {
-    marginTop: 20,
-    fontSize: 20,
-    fontWeight:'bold'
-  }
-});
+}
 
 export default App;
